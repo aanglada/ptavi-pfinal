e538b34d30b7c6e5d3f1b3b00521ff8432de6fac
Antonio <anto.nunez@hotmail.com>
2019-01-09 13:24:47 +0100

    ya se tratan los invites en uaserver

--- uaserver.py
+++ uaserver.py
@@ -11,31 +11,45 @@ from uaclient import XMLHandler
 from proxy_registrar import Log
 
 usage_error = 'Usage: python uaserver.py config'
-TRYING = b"SIP/2.0 100 Trying\r\n"
-RINGING = b"SIP/2.0 180 Ringing\r\n"
-OK = b"SIP/2.0 200 OK\r\n"
-BAD_REQUEST = b"SIP/2.0 400 Bad Request\r\n"
-Not_Allowed = b"SIP/2.0 405 Method Not Allowed\r\n"
-aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < " 
+trying = "SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ringing\r\n\r\nSIP/2.0 200 OK\r\n"
+bad_request = "SIP/2.0 400 Bad Request\r\n"
+not_allowed = "SIP/2.0 405 Method Not Allowed\r\n"
+aEjecutar = "./mp32rtp -i ip -p port < audio" 
 
 class SIPHandler(socketserver.DatagramRequestHandler):
+    mp32rtp = []
 
     def handle(self):
         message = self.rfile.read().decode('utf-8')
-        METHOD = self.line[0]
-        if METHOD == 'INVITE':
-            print('INVITE received')
-            self.wfile.write(TRYING + b'\r\n' + RINGING + b'\r\n'
-                             + OK + b'\r\n')
-        elif METHOD == 'ACK':
-            print("ACK RECIVED")
-            os.system(aEjecutar)
-        elif METHOD == 'BYE':
-            print("BYE RECIVED")
-            self.wfile.write(OK + b'\r\n')
+        method = message.split()[0]
+        print(method, 'received')
+        if method == 'INVITE':
+            sdp = message.split('\r\n')[1:]
+            sdp_body = ''
+            for line in sdp:
+                if 'o=' in line:
+                    user = line.split('=')[1].split()[0]
+                    ip = line.split('=')[1].split()[1]
+                    self.mp32rtp.append(ip)
+                    new_line = line.replace(user, config['account_username']).replace(ip, config['uaserver_ip'])
+                    sdp_body += new_line + '\r\n'
+                elif 'm=' in line:
+                    rtpaudio = line.split()[1]
+                    self.mp32rtp.append(rtpaudio)
+                    sdp_body += line.replace(rtpaudio, config['rtpaudio_puerto']) + '\r\n'
+                else:
+                    sdp_body += line + '\r\n'
+            response = trying + sdp_body
+            print(response)
+            self.wfile.write(bytes(response, 'utf-8'))
+        elif method == 'ACK':
+            comando = aEjecutar.replace('ip', self.mp32rtp[0]).replace('port', self.mp32rtp[1])
+            comando = comando.replace('audio', config['audio_path'])
+            os.system(comando)
+        elif method == 'BYE':
+            self.wfile.write(bytes(ok, 'utf-8'))
         else:
-            print("METHOD NOT ALLOWED")
-            self.wfile.write(Not_Allowed + b'\r\n')
+            self.wfile.write(bytes(not_allowed, 'utf-8'))
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
