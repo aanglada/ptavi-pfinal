8c4bd0eb38f50432036186791ed22b3317f4ae14
Antonio <anto.nunez@hotmail.com>
2019-01-09 14:14:13 +0100

    pasado pep8

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -17,7 +17,8 @@ RINGING = b"SIP/2.0 180 Ringing\r\n"
 OK = b"SIP/2.0 200 OK\r\n"
 BAD_REQUEST = b"SIP/2.0 400 Bad Request\r\n"
 Not_Allowed = b"SIP/2.0 405 Method Not Allowed\r\n"
-aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < " 
+aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < "
+
 
 def digest_nonce(server_name, ip):
     d = hashlib.sha512()
@@ -25,11 +26,13 @@ def digest_nonce(server_name, ip):
     d.digest()
     return d.hexdigest()
 
+
 def digest_response(nonce, username, passwd):
     d = hashlib.sha512()
     d.update(bytes(nonce + username + passwd, 'utf-8'))
     d.digest()
     return d.hexdigest()
+
 
 class Log:
 
@@ -39,21 +42,21 @@ class Log:
         self.path = path
 
     def write(self, msg):
-        with open(self.path,'a') as logfile:
+        with open(self.path, 'a') as logfile:
             now = datetime.now().strftime('%Y%m%d%H%M%S')
-            logfile.write(now,msg)
+            logfile.write(now, msg)
 
     def starting(self):
         msg = 'Starting...\n'
         self.write(msg)
 
     def sent_to(self, ip, port, mess):
-        m = mess.replace('\r\n',' ')
+        m = mess.replace('\r\n', ' ')
         msg = 'Sent to ' + ip + ':' + str(port) + ': ' + m + '\n'
         self.write(msg)
 
     def received_from(self, ip, port, mess):
-        m = mess.replace('\r\n',' ')
+        m = mess.replace('\r\n', ' ')
         msg = 'Received from ' + ip + ':' + str(port) + ': ' + m + '\n'
         self.write(msg)
 
@@ -69,18 +72,19 @@ class Log:
 class XMLHandler(ContentHandler):
 
     def __init__(self):
-        self.dicc = {'server':{'name':'','ip':'','puerto':''},
-                     'database':{'path':'','passwdpath':''},
-                     'log':{'path':''}}
+        self.dicc = {'server': {'name': '', 'ip': '', 'puerto': ''},
+                     'database': {'path': '', 'passwdpath': ''},
+                     'log': {'path': ''}}
         self.data = {}
 
     def startElement(self, name, attrs):
         if name in self.dicc:
             for att in self.dicc[name]:
-                self.data[name + '_' + att] = attrs.get(att,'')
+                self.data[name + '_' + att] = attrs.get(att, '')
 
     def get_tags(self):
         return self.data
+
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
@@ -96,30 +100,42 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         if method == 'REGISTER':
             user = message.split()[1].split(':')[1]
             if user in self.dicc:
-                expires = message.split('\n')[1].split(':')[1]
+                exp = message.split('\n')[1].split(':')[1]
                 if int(expires) == 0:
                     del self.dicc[user]
                 else:
-                    expires_time = (datetime.now() + timedelta(seconds=int(expires))).strftime('%H:%M:%S %d-%m-%Y')
-                    self.dicc[user]['expires'] = expires_time
+                    exp_time = datetime.now() + timedelta(seconds=int(exp))
+                    exp_date = exp_time.strftime('%H:%M:%S %d-%m-%Y')
+                    self.dicc[user]['expires'] = exp_date
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
             else:
                 if 'Authorization' in message:
                     resp_user = message.split('\n')[2].split('"')[1]
-                    nonce = digest_nonce(config['server_name'], config['server_ip'])
+                    server_name = config['server_name']
+                    server_ip = config['server_ip']
+                    nonce = digest_nonce(server_name, server_ip)
                     username = message.split('\n')[0].split(':')[1]
-                    response = digest_response(nonce, username, self.passwd[username])
+                    passwd = self.passwd[username]
+                    response = digest_response(nonce, username, passwd)
                     if resp_user == response:
                         expires = message.split('\n')[1].split(':')[1]
-                        address = self.client_address[0] + ':' + message.split()[1].split(':')[2]
-                        expires_time = (datetime.now() + timedelta(seconds=int(expires))).strftime('%H:%M:%S %d-%m-%Y')
-                        self.dicc[user] = {'address': address, 'expires': expires_time}
+                        ip = self.client_address[0]
+                        port = message.split()[1].split(':')[2]
+                        address = ip + ':' + port
+                        now = datetime.now()
+                        exp_time = now + timedelta(seconds=int(expires))
+                        exp_date = exp_time.strftime('%H:%M:%S %d-%m-%Y')
+                        self.dicc[user] = {'address': address,
+                                           'expires': expires_time}
                         self.wfile.write(b'SIP/2.0 200 OK\r\n')
                     else:
                         self.wfile.write(b'SIP/2.0 400 Bad Request\r\n')
                 else:
-                    nonce = digest_nonce(config['server_name'], config['server_ip'])
-                    mess = 'SIP/2.0 401 Unauthorized\nWWW Authenticate: Digest nonce="'+ nonce +'"\r\n'
+                    server_name = config['server_name']
+                    server_ip = config['server_ip']
+                    nonce = digest_nonce(server_name, server_ip)
+                    mess = 'SIP/2.0 401 Unauthorized\nWWW Authenticate: ' + \
+                           'Digest nonce="' + nonce + '"\r\n'
                     self.wfile.write(bytes(mess, 'utf-8'))
         elif method == 'INVITE':
             user_src = message.split('\r\n')[4].split()[0].split('=')[1]
@@ -198,8 +214,9 @@ if __name__ == "__main__":
     parser.setContentHandler(cHandler)
     parser.parse(open(xmlfile))
     config = cHandler.get_tags()
-    server = socketserver.UDPServer((config['server_ip'], int(config['server_puerto'])), SIPRegisterHandler)
-    
+    server_address = (config['server_ip'], int(config['server_puerto']))
+    server = socketserver.UDPServer(server_address, SIPRegisterHandler)
+
     print('Listening...')
     try:
         server.serve_forever()
